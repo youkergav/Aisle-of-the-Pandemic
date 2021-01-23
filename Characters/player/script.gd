974ce97d232a0bbe06b5d514e2dca4110c1411ab
Gavin Youker <youkergav@gmail.com>
2021-01-23 17:37:43 -0500

    Added walking animations

--- Characters/player/script.gd
+++ Characters/player/script.gd
@@ -2,23 +2,49 @@ extends KinematicBody2D
 
 var speed: int = 200
 var velocity: Vector2 = Vector2.ZERO
+onready var animation = $AnimationPlayer
 
-func _physics_process(delta):
+func walk():
 	velocity.x = 0
 	velocity.y = 0
 	
 	if Input.is_action_pressed("move_left"):
+		animation.play("WalkLeft")
 		velocity.x -= speed
 		
 	if Input.is_action_pressed("move_right"):
+		animation.play("WalkRight")
 		velocity.x += speed
 		
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_up"): 
+		animation.play("WalkUp")
 		velocity.y -= speed
 		
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("move_down"): 
+		animation.play("WalkDown")
 		velocity.y += speed
-		
-	var collision = move_and_slide(velocity, Vector2.UP)
 	
+	var collision = move_and_slide(velocity, Vector2.UP)
 	print(collision)
+	
+func idle():
+	if Input.is_action_just_released("move_left"):
+		animation.play("IdleLeft")
+		
+	if Input.is_action_just_released("move_right"):
+		animation.play("IdleRight")
+		
+	if Input.is_action_just_released("move_up"): 
+		animation.play("IdleUp")
+		
+	if Input.is_action_just_released("move_down"): 
+		animation.play("IdleDown")
+		
+func spin():
+	if Input.is_action_just_released("spin"): 
+		animation.play("Spin")
+	
+func _physics_process(delta):
+	walk()
+	idle()
+	spin()
