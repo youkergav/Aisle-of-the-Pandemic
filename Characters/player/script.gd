eeae91a6bbccc9afbef91b9f8ecc0e78d9e12bfd
Gavin Youker <youkergav@gmail.com>
2021-01-23 19:01:33 -0500

    Added first NPC animation

--- Characters/player/script.gd
+++ Characters/player/script.gd
@@ -5,46 +5,56 @@ var velocity: Vector2 = Vector2.ZERO
 onready var animation = $AnimationPlayer
 
 func walk():
-	velocity.x = 0
-	velocity.y = 0
-	
-	if Input.is_action_pressed("move_left"):
-		animation.play("WalkLeft")
-		velocity.x -= speed
-		
-	if Input.is_action_pressed("move_right"):
-		animation.play("WalkRight")
-		velocity.x += speed
-		
-	if Input.is_action_pressed("move_up"): 
-		animation.play("WalkUp")
-		velocity.y -= speed
-		
-	if Input.is_action_pressed("move_down"): 
-		animation.play("WalkDown")
-		velocity.y += speed
-	
-	var collision = move_and_slide(velocity, Vector2.UP)
-	print(collision)
-	
+    velocity.x = 0
+    velocity.y = 0
+    
+    if Input.is_action_pressed("move_left"):
+        animation.play("WalkLeft")
+        velocity.x -= speed
+        
+    if Input.is_action_pressed("move_right"):
+        animation.play("WalkRight")
+        velocity.x += speed
+        
+    if Input.is_action_pressed("move_up"): 
+        animation.play("WalkUp")
+        velocity.y -= speed
+        
+    if Input.is_action_pressed("move_down"): 
+        animation.play("WalkDown")
+        velocity.y += speed
+    
+    var collision = move_and_slide(velocity, Vector2.UP)
+    
 func idle():
-	if Input.is_action_just_released("move_left"):
-		animation.play("IdleLeft")
-		
-	if Input.is_action_just_released("move_right"):
-		animation.play("IdleRight")
-		
-	if Input.is_action_just_released("move_up"): 
-		animation.play("IdleUp")
-		
-	if Input.is_action_just_released("move_down"): 
-		animation.play("IdleDown")
-		
+    if Input.is_action_just_released("move_left"):
+        animation.play("IdleLeft")
+        
+    if Input.is_action_just_released("move_right"):
+        animation.play("IdleRight")
+        
+    if Input.is_action_just_released("move_up"): 
+        animation.play("IdleUp")
+        
+    if Input.is_action_just_released("move_down"): 
+        animation.play("IdleDown")
+        
 func spin():
-	if Input.is_action_just_released("spin"): 
-		animation.play("Spin")
-	
+    if Input.is_action_just_released("spin"): 
+        animation.play("Spin")
+    
 func _physics_process(delta):
-	walk()
-	idle()
-	spin()
+    walk()
+    idle()
+    spin()
+
+#func _on_timeout():
+#    var shadow = preload("res://Lighting/Shadow/Shadow.tscn")
+#    get_parent().add_child(shadow)
+#
+#    var player_frame = $Player/Sprite.frame
+#    var offset = player_frame * .100
+#
+#    print(offset)
+#    shadow.position = position
+#    # shadow.texture = ??????????????
